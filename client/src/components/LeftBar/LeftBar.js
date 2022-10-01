2c82fde29d32289ec37e42e42d89d121d0e7ce91
Hans Pattnaik <pattnaikhans@gmail.com>
2022-10-01 16:05:56 +0530

    Fixed LeftBar.js

--- client/src/components/LeftBar/LeftBar.js
+++ client/src/components/LeftBar/LeftBar.js
@@ -12,21 +12,21 @@ function LeftBar(props) {
     const [dsquestions, setDSQuestions] = useState(DSQuestions);
     const [focquestions, setFOCQuestions] = useState(FocQuestions);
     const [pythonquestion, setPYTHONQuestions] = useState(PythonQuestions);
-    
-   
+    var hasRendered = false;
+
 
     const addTag = (newTag) => {
         setTag(newTag);
-        if(props.name === "Data Structures"){
+        if (props.name === "Data Structures") {
             let temp = DSQuestions.filter(item => item.PreReq.includes(newTag.replace(/\s+/g, '')));
             setDSQuestions(temp);
         }
         setTag(newTag);
-        if(props.name === "Python Laboratory"){
-           let temp = PythonQuestions.filter(item => item.PreReq.includes(newTag.replace(/\s+/g, '')));
+        if (props.name === "Python Laboratory") {
+            let temp = PythonQuestions.filter(item => item.PreReq.includes(newTag.replace(/\s+/g, '')));
             setPYTHONQuestions(temp);
         }
-        else{
+        else {
             let temp = FocQuestions.filter(item => item.PreReq.includes(newTag.replace(/\s+/g, '')));
             setFOCQuestions(temp);
         }
@@ -34,9 +34,9 @@ function LeftBar(props) {
 
     const removeTag = () => {
         setTag(null);
-        if(props.name === "Data Structures")
+        if (props.name === "Data Structures")
             setDSQuestions(DSQuestions);
-        else if(props.name === "Python Laboratory")
+        else if (props.name === "Python Laboratory")
             setPYTHONQuestions(PythonQuestions);
         else
             setFOCQuestions(FocQuestions);
@@ -50,79 +50,83 @@ function LeftBar(props) {
                     {props.arr.map((title) => (
                         <LeftBarItem key={title} name={title} tag={tag} addTag={addTag} removeTag={removeTag} />
                     ))}
-                </div>    
+                </div>
             </div>
-           if(props.name === "Data Structures"){
-           <div>
-            <div className={styles.qContainer}>
-                {tag && <div className={styles.dsPage}>
-                    <Link to={`/datastructure/${tag.replace(/\s+/g, '')}`} style={{ all: 'unset' }}>
-                        <h2>Learn about {tag} &#8921;</h2>
-                    </Link>
-                </div>}
-                {(tag === 'Tree' || tag === 'Graph') && <div className={styles.graph}>
-                    <a href="https://graph-visualizer-labxrit.herokuapp.com" target="_blank" rel="noreferrer">
-                        <h2>&#8920; {tag} Visualizer &#8921;</h2>
-                    </a>
-                </div>}
-                {dsquestions.map((question) => (
-                    <Link to={`/dslab/question/${question.id}`} style={{ all: 'unset' }} key={question.id}>
-                        <Question
-                            id={question.id}
-                            question={question.value}
-                        />
-                    </Link>
-                ))
-                }
-            </div>
-            </div>
+            {
+                (props.name === 'Data Structures') &&
+                (hasRendered = true) &&
+                <div>
+                    <div className={styles.qContainer}>
+                        {tag && <div className={styles.dsPage}>
+                            <Link to={`/datastructure/${tag.replace(/\s+/g, '')}`} style={{ all: 'unset' }}>
+                                <h2>Learn about {tag} &#8921;</h2>
+                            </Link>
+                        </div>}
+                        {(tag === 'Tree' || tag === 'Graph') && <div className={styles.graph}>
+                            <a href="https://graph-visualizer-labxrit.herokuapp.com" target="_blank" rel="noreferrer">
+                                <h2>&#8920; {tag} Visualizer &#8921;</h2>
+                            </a>
+                        </div>}
+                        {dsquestions.map((question) => (
+                            <Link to={`/dslab/question/${question.id}`} style={{ all: 'unset' }} key={question.id}>
+                                <Question
+                                    id={question.id}
+                                    question={question.value}
+                                />
+                            </Link>
+                        ))
+                        }
+                    </div>
+                </div>
             }
-            else if(props.name === "Python Laboratory"){
-           <div>
-            <div className={styles.qContainer}>
-                {tag && <div className={styles.dsPage}>
-                    <Link to={`/pythonlab/${tag.replace(/\s+/g, '')}`} style={{ all: 'unset' }}>
-                        <h2>Learn about {tag} &#8921;</h2>
-                    </Link>
-                </div>}
-                {/* {{(tag === 'Tree' || tag === 'Graph') && <div className={styles.graph}>
+            {
+                (props.name === 'Python Laboratory') &&
+                (hasRendered = true) &&
+                <div>
+                    <div className={styles.qContainer}>
+                        {tag && <div className={styles.dsPage}>
+                            <Link to={`/pythonlab/${tag.replace(/\s+/g, '')}`} style={{ all: 'unset' }}>
+                                <h2>Learn about {tag} &#8921;</h2>
+                            </Link>
+                        </div>}
+                        {/* {{(tag === 'Tree' || tag === 'Graph') && <div className={styles.graph}>
                     <a href="https://graph-visualizer-labxrit.herokuapp.com" target="_blank" rel="noreferrer">
                         <h2>&#8920; {tag} Visualizer &#8921;</h2>
                     </a>
             </div>}} */}
-                {pythonquestion.map((question) => (
-                    <Link to={`/pythonlab/question/${question.id}`} style={{ all: 'unset' }} key={question.id}>
-                        <Question
-                            id={question.id}
-                            question={question.value}
-                        />
-                    </Link>
-                ))
-}
-            </div>
-            </div>
-} 
-
-            else{
-            <div>
-            <div className={styles.qContainer}>
-                {tag && <div className={styles.dsPage}>
-                    <Link to={`/foclab/${tag.replace(/\s+/g, '')}`} style={{ all: 'unset' }}>
-                        <h2>Learn about {tag} &#8921;</h2>
-                    </Link>
-                </div>}
-                {focquestions.map((question) => (
-                    <Link to={`/foclab/question/${question.id}`} style={{ all: 'unset' }} key={question.id}>
-                        <Question
-                            id={question.id}
-                            question={question.value}
-                        />
-                    </Link>
-                ))
-                }
-        </div>
-        </div>
-}
+                        {pythonquestion.map((question) => (
+                            <Link to={`/pythonlab/question/${question.id}`} style={{ all: 'unset' }} key={question.id}>
+                                <Question
+                                    id={question.id}
+                                    question={question.value}
+                                />
+                            </Link>
+                        ))
+                        }
+                    </div>
+                </div>
+            }
+            {
+                (hasRendered === false) &&
+                <div>
+                    <div className={styles.qContainer}>
+                        {tag && <div className={styles.dsPage}>
+                            <Link to={`/foclab/${tag.replace(/\s+/g, '')}`} style={{ all: 'unset' }}>
+                                <h2>Learn about {tag} &#8921;</h2>
+                            </Link>
+                        </div>}
+                        {focquestions.map((question) => (
+                            <Link to={`/foclab/question/${question.id}`} style={{ all: 'unset' }} key={question.id}>
+                                <Question
+                                    id={question.id}
+                                    question={question.value}
+                                />
+                            </Link>
+                        ))
+                        }
+                    </div>
+                </div>
+            }
         </div>
 
     );
